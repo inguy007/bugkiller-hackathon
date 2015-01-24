0feb5068772830639be4649530890c0fb913474d
indrajit <inguy007@gmail.com>
2015-01-24 15:04:22 +0530

    added mapreduce for multivariate frequency distribution

---
+++ Outlier-Detector/src/com/bugkiller/common/util/FieldDataTypes.java
@@ -0,0 +1,8 @@
+package com.bugkiller.common.util;
+
+public class FieldDataTypes {
+
+	public static final String INTEGER = "INTEGER";
+	public static final String STRING = "STRING";
+
+}
--- Outlier-Detector/src/com/bugkiller/distribution/mr/BucketingDriver.java
+++ Outlier-Detector/src/com/bugkiller/distribution/mr/BucketingDriver.java
@@ -1,34 +1,44 @@
 package com.bugkiller.distribution.mr;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
+import com.bugkiller.common.util.ConfigurationUtils;
+
 public class BucketingDriver {
 
+	static Path inputPath = new Path("/test/mapreduce/hackathon/input.csv");
+	static Path outputPath = new Path("/test/mapreduce/hackathon/output");
+	static String metaFilePath = "/test/mapreduce/hackathon/meta.json";
+	
 	public static void main(String[] args) throws Exception {
-		Configuration conf = new Configuration();
-		Job job = new Job(conf, "outlier-bucketing");
-		job.setJarByClass(com.bugkiller.distribution.mr.BucketingDriver.class);
-		job.setMapperClass(com.bugkiller.distribution.mr.BucketingMapper.class);
-
-		job.setReducerClass(com.bugkiller.distribution.mr.BucketingReducer.class);
-
-		// TODO: specify output types
+		Configuration conf = ConfigurationUtils.getConfiguration();
+		conf.setInt("frequencyThreshold",5);
+		conf.set("field.record.delim", "|");
+        conf.set("metadata.file.path", ",");
+		Job job = new Job(conf, "Csv_Column_Counter");
+		job.setJarByClass(BucketingDriver.class);
+		job.setMapperClass(BucketingMapper.class);
+		job.setReducerClass(BucketingReducer.class);
 		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(Text.class);
-
+		job.setOutputValueClass(IntWritable.class);
+		
+		FileSystem fs = FileSystem.get(conf);
+		if(fs.exists(outputPath)){
+			fs.delete(outputPath, true);
+		}
 		// TODO: specify input and output DIRECTORIES (not files)
-		FileInputFormat.setInputPaths(job, new Path("src"));
-		FileOutputFormat.setOutputPath(job, new Path("out"));
-
-		if (!job.waitForCompletion(true))
-			return;
+		FileInputFormat.setInputPaths(job, inputPath);
+		FileOutputFormat.setOutputPath(job, outputPath);
+		System.out.println("Starting Job");
+		job.waitForCompletion(true);
+		System.out.println("Job Completed");	
 	}
 
 }
