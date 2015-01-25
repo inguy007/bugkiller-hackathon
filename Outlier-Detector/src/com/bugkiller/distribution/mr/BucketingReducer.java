3c4fd29aaa5638f8f4a05082bb6a98a60d4a85f4
indrajit <inguy007@gmail.com>
2015-01-25 15:51:04 +0530

    the one which has the lowest score contributes to the overall score

--- Outlier-Detector/src/com/bugkiller/distribution/mr/BucketingReducer.java
+++ Outlier-Detector/src/com/bugkiller/distribution/mr/BucketingReducer.java
@@ -10,7 +10,6 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
 
 import com.bugkiller.common.util.ComputationUtility;
 import com.bugkiller.distribution.NormalizedRecord;
@@ -18,7 +17,7 @@ import com.bugkiller.distribution.NormalizedRecord;
 public class BucketingReducer extends Reducer<NormalizedRecord, Text, NullWritable, NormalizedRecord> {
 
 	 private int frequencyThreshold;
-	 static List<NormalizedRecord> corpus= new ArrayList<NormalizedRecord>();
+	 static List<NormalizedRecord> highFreqBuckets= new ArrayList<NormalizedRecord>();
 	 static List<NormalizedRecord> lowFreqBuckets = new ArrayList<NormalizedRecord>();
 
 	 
@@ -26,7 +25,7 @@ public class BucketingReducer extends Reducer<NormalizedRecord, Text, NullWritab
 	protected void setup(Context context) throws IOException, InterruptedException {
 		Configuration conf = context.getConfiguration();
      	frequencyThreshold = conf.getInt("frequencyThreshold",3);
-     	corpus = new ArrayList<NormalizedRecord>();
+     	highFreqBuckets = new ArrayList<NormalizedRecord>();
      	lowFreqBuckets = new ArrayList<NormalizedRecord>();
      }
 		
@@ -40,7 +39,7 @@ public class BucketingReducer extends Reducer<NormalizedRecord, Text, NullWritab
 		if(frequency <= frequencyThreshold){
 			lowFreqBuckets.add(key.createClone());
 		}else{
-			corpus.add(key.createClone());
+			highFreqBuckets.add(key.createClone());
 		}
 	}
 	
@@ -62,7 +61,7 @@ public class BucketingReducer extends Reducer<NormalizedRecord, Text, NullWritab
 				lowFreqPositionValueMap.put(Integer.parseInt(fieldValue.split("~")[0]), fieldValue.split("~")[1]);
 			}
 		}
-		for(NormalizedRecord highFreqRecord : corpus){
+		for(NormalizedRecord highFreqRecord : highFreqBuckets){
 			Map<Integer,String> highFreqPositionValueMap = new HashMap<Integer,String>();
 			List<Object> highFreqRecordFields = highFreqRecord.getFields();
 			for(Object fieldObj : highFreqRecordFields){
@@ -71,27 +70,25 @@ public class BucketingReducer extends Reducer<NormalizedRecord, Text, NullWritab
 					highFreqPositionValueMap.put(Integer.parseInt(fieldValue.split("~")[0]), fieldValue.split("~")[1]);
 				}
 			}
-			//double stringSimilarityScore = 0;
-			//double numericSimilarityScore = 0;
 			double score =0;
-			double n = 0;
 			for(Integer ordinalPosition : lowFreqPositionValueMap.keySet()){
+				double computedScore = 0;
 				String src = lowFreqPositionValueMap.get(ordinalPosition);
 				String target = highFreqPositionValueMap.get(ordinalPosition);
 				if(!src.matches("\\d+") && !target.matches("\\d+")){
-					System.out.println("Score between :"+src+" and target :"+target+" is "+computeStringScore(src,target));
-					score += computeStringScore(src, target);	
+					computedScore = computeStringScore(src, target);
 				}else{
-					System.out.println("Score between :"+src+" and target :"+target+" is "+computeNumericSimilarityScore(src,target));
-					score += computeNumericSimilarityScore(src,target);
+					computedScore = computeNumericSimilarityScore(src, target);
 				}
-				n++;
+				if(score != 0){
+					if(computedScore < score){
+						score = computedScore;
+					}
+				}else{
+					score = computedScore;
+				}
 			}
-			score = score/n;
 			if(similarityScore == 0 || score > similarityScore){
-				if(score >= 0.8){
-					System.out.println("BEST MATCHES ::"+lowFreqRecord.createClone()+" and "+highFreqRecord.createClone()+" with score :"+score);	
-				}
 				similarityScore = score;
 			}
 		}
@@ -107,7 +104,6 @@ public class BucketingReducer extends Reducer<NormalizedRecord, Text, NullWritab
 
 	private double computeStringScore(String first, String second) {
 		int maxLength = Math.max(first.length(), second.length());
-		// Can't divide by 0
 		if (maxLength == 0)
 			return 1.0d;
 		return ((double) (maxLength - ComputationUtility.findStringDistance(first, second)))/ (double) maxLength;
